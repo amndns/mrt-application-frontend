d53e1767c1980e38348c97cbec2b85e0bc4ba5c3
Adel Mandanas <mandanas.adel@gmail.com>
2017-07-15 18:08:11 +0800

    Add a Query component with dropdown elements

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Query } from './Query'
 
 export class Main extends React.Component {
-  render(){
-    return(
+  constructor(props) {
+    super(props);
+    this.state = {
+      station: 1, // Represents the North Avenue Station
+      direction: 0, // Represents the direction being South
+      time: 0 // Represents the time at 5 AM
+    };
+  }
+
+  render() {
+    return (
       <div>
-        <h1>Hello world!</h1>
+        <Query />
       </div>
     );
   }
--- tests/test_Main.js
+++ tests/test_Main.js
@@ -3,13 +3,27 @@ import React from 'react'
 import { shallow }  from 'enzyme'
 
 import { Main } from '../src/Components/Main'
+import { Query } from '../src/Components/Query'
 let expect = chai.expect
 
 describe('<Main />', () => {
-
-  it('should render the Main Component', () => {
+  it('should have a staion state', () => {
     const wrapper = shallow(<Main />);
-    expect(wrapper).to.have.length(1);
+    expect(wrapper.state('station')).to.equal(1);
   });
 
+  it('should have a direction state', () => {
+    const wrapper = shallow(<Main />);
+    expect(wrapper.state('direction')).to.equal(0);
+  });
+
+  it('should have a time state', () => {
+    const wrapper = shallow(<Main />);
+    expect(wrapper.state('time')).to.equal(0);
+  });
+
+  it('should render the Query component', () => {
+    const wrapper = shallow(<Main />);
+    expect(wrapper.find(Query)).to.have.length(1);
+  });
 });
